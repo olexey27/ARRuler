012af3049b9af718bd88e18167044be89e5f1ba9
olexey27 <allxyog@gmail.com>
2024-09-17 18:40:53 +0200

    Initial Commit

---
+++ ARRulerTests/ARRulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARRulerTests.swift
+//  ARRulerTests
+//
+//  Created by Alexej K on 17/09/2024.
+//
+
+import Testing
+@testable import ARRuler
+
+struct ARRulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
